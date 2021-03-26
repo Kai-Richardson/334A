6521519ef2300e46f2d6dc7089b13701c17b43f3
Kai Richardson <richardk22@up.edu>
2021-03-26 16:21:40 -0700

    HW3: Threaded mutex-locked buffered writing

--- assn3/src/main.rs
+++ assn3/src/main.rs
@@ -1,6 +1,10 @@
 use argh::FromArgs;
-use std::{fs::File, io::{BufWriter, Write}};
-use tokio::spawn;
+use std::{
+    fs::File,
+    io::{BufWriter, Write},
+    sync::{Arc, Mutex},
+    thread
+};
 use tokio::sync::Semaphore;
 
 #[derive(Debug, FromArgs)]
@@ -19,38 +23,43 @@ struct Arguments {
     output_file: String,
 }
 
-#[tokio::main]
-async fn main() {
+fn main() {
     let args: Arguments = argh::from_env();
 
     // Setup output file
-    let output_file = File::open(args.output_file).expect("Error encountered opening the output file.");
-    let mut output = BufWriter::new(output_file); // Buffered writing
+    let output_file = File::create(args.output_file).expect("Error opening output file.");
+    let writer = BufWriter::new(output_file); // Buffered writing
+    let counter = Arc::new(Mutex::new(writer));
+
     // write!(output, "foo").expect("Failure writing");
-    
 
     // Semaphore to only let us spawn so many threads
     let sem = Semaphore::new(args.thread_count);
+    /*
+        iterate over file names
+        dec sema / spawn thread
+        do work - need mutex on output file writing
+        inc sema
+    */
+    let _ = sem.acquire();
 
-    println!("Hello, world!");
+    let mut handles = vec![];
 
-    // iterate over file names
-    // dec sema / spawn thread
-    // do work - need mutex on output file writing
-    // inc sema
+    for _ in 0..=10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut mutex = counter.lock().expect("error obtaining mutex lock");
 
-    let _ = sem.acquire().await;
-
-    for _ in 0..10 {
-        let task = spawn(write_to_output());
-        task.await.unwrap();
+            mutex.write_all(b"foo").unwrap();
+            mutex.flush().unwrap();
+        });
+        handles.push(handle);
+    }
+    for handle in handles {
+        handle.join().unwrap();
     }
 
-    println!("Exiting...");
-}
+    //println!("Result: {}", *counter.lock().unwrap());
 
-async fn write_to_output() {
-    println!("Foo");
-    tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
-    println!("Async task done.");
+    println!("Exiting...");
 }
